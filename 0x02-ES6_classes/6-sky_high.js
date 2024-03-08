887aca5f381ef5e13621dddf957fc8dbc5a45da0
Xage1 <kennethkiarie555@gmail.com>
2024-03-08 12:12:39 +0000

    Another

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building.js';
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Call the constructor of the parent class with the 'sqft' parameter
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
+
+export default SkyHighBuilding;

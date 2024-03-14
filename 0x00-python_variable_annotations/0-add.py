cf67cb436f52cfd59e526bace9d2c23cb5341f4e
Xage1 <kennethkiarie555@gmail.com>
2024-03-14 11:42:11 +0000

    this

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/env python3
+
 """Task0 for today"""
+
+
 def add(a: float, b: float) -> float:
     """Adds two floats and returns their sum."""
-    return a +b
+    return a + b

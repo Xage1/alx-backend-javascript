2f239b9b611fd6d9d6475e54ea24b8d1c94058b8
Xage1 <kennethkiarie555@gmail.com>
2024-04-29 17:53:15 +0000

    This is actually Awesome

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,37 +1,37 @@
 const fs = require('fs');
 
 function countStudents(path) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(path, 'utf8', (err, data) => {
-			if (err) {
-				reject(new Error('Cannot load the database'));
-			} else {
-				const lines = data.split('\n').filter(line => line.trim() !== '');
-				if (lines.length === 0) {
-					reject(new Error('Empty database'));
-				}
-				const students = lines.map(line => line.split(','));
-				const fields = {};
-				students.forEach(student => {
-					const field = student[3];
-					if (field in fields) {
-						fields[field].push(student[0]);
-					} else {
-						fields[field] = [student[0]];
-					}
-				});
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        if (lines.length === 0) {
+          reject(new Error('Empty database'));
+        }
+        const students = lines.map((line) => line.split(','));
+        const fields = {};
+        students.forEach((student) => {
+          const field = student[3];
+          if (field in fields) {
+            fields[field].push(student[0]);
+          } else {
+            fields[field] = [student[0]];
+          }
+        });
 
-				let totalStudents = 0;
-				Object.entries(fields).forEach(([field, names]) => {
-					totalStudents += names.length;
-					console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-				});
+        let totalStudents = 0;
+        Object.entries(fields).forEach(([field, names]) => {
+          totalStudents += names.length;
+          console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+        });
 
-				console.log(`Number of students: ${totalStudents}`);
-				resolve();
-			}
-		});
-	});
+        console.log(`Number of students: ${totalStudents}`);
+        resolve();
+      }
+    });
+  });
 }
 
 module.exports = countStudents;
--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,19 +1,19 @@
 const http = require('http');
 
-//create a http server
+// create a http server
 const app = http.createServer((req, res) => {
-	//Set response status code to 200 meaning OKAY
-	res.statusCode = 200;
+  // Set response status code to 200 meaning OKAY
+  res.statusCode = 200;
 
-	//Set the content type to plain text
-	res.setHeader('Content-Type', 'text/plain');
+  // Set the content type to plain text
+  res.setHeader('Content-Type', 'text/plain');
 
-	//Send the response Body
-	res.end('Hello Holberton School!\n');
+  // Send the response Body
+  res.end('Hello Holberton School!\n');
 });
 
-//Listen on pert 1245
+// Listen on pert 1245
 app.listen(1245);
 
-//Export the app variable
+// Export the app variable
 module.exports = app;

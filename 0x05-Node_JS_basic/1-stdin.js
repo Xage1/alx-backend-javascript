d935944896270542c6c2ec265a1de2fc080c889c
Xage1 <kennethkiarie555@gmail.com>
2024-04-29 16:24:02 +0000

    This is actually Awesome

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+	const name = data.trim();
+
+	if (name) {
+		process.stdout.write(`Your name is: ${name}\n`);
+		process.stdout.write('This important software is now closing\n');
+		process.exit();
+	}
+});
+
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing\n');
+	process.exit();
+});

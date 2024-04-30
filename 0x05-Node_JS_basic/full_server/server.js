5868562a0235ae0b914988bcacb3d3e817423397
Xage1 <kennethkiarie555@gmail.com>
2024-04-30 16:29:42 +0000

    This Update👍

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./routes/index.js');
+const router = require('./routes/index');
 
 const app = express();
 const PORT = 1245;
@@ -9,7 +9,7 @@ app.use('/students', router);
 app.use('/students/:major', router);
 
 app.listen(PORT, () => {
-	console.log(`...... Connecting on port ${PORT}`);
+  console.log(`...... Connecting on port ${PORT}`);
 });
 
-export default app;
+module.exports = app;
